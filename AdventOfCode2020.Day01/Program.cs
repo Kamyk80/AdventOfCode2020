4a83da38fe33c62fdf3328bc44636f52a226a007
Kamyk80 <45560042+Kamyk80@users.noreply.github.com>
2020-12-01 18:15:10 +0100

    Day 01

--- AdventOfCode2020.Day01/Program.cs
+++ AdventOfCode2020.Day01/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2020.Day01
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var entries = File.ReadAllLines("input.txt")
+                .Select(int.Parse)
+                .ToList();
+
+            var two = entries
+                .SelectMany(x => entries, (x, y) => new {x, y})
+                .Where(e => e.x + e.y == 2020)
+                .Select(e => e.x * e.y)
+                .First();
+
+            var three = entries
+                .SelectMany(x => entries, (x, y) => new {x, y})
+                .SelectMany(e => entries, (e, z) => new {e.x, e.y, z})
+                .Where(e => e.x + e.y + e.z == 2020)
+                .Select(e => e.x * e.y * e.z)
+                .First();
+
+            Console.WriteLine(two);
+            Console.WriteLine(three);
+            Console.ReadKey(true);
         }
     }
 }
